449e27ea1725344b40878696c0298f6905dd8b5a
Alexander Pyreev <PyreevAS@msk.bcs.ru>
2019-02-22 13:36:28 +0300

    lesson 29 - props

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -6,10 +6,10 @@ const TodoList = () => {
   return (
     <ul>
       <li>
-        <TodoListItem />
+        <TodoListItem label="Drink Coffee" />
       </li>
       <li>
-        <TodoListItem />
+        <TodoListItem label="Build React App" important />
       </li>
     </ul>
   );
