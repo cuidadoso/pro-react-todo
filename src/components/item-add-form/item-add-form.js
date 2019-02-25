472b6cda7a9450f3d3dcf66a0d806ed9154055af
Alexander Pyreev <PyreevAS@msk.bcs.ru>
2019-02-25 13:23:45 +0300

    lesson 44 - form implementation

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,17 +3,34 @@ import React, { Component } from 'react';
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value
+    });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onItemAdded(this.state.label);
+  };
+
+  state = {
+    label: ''
+  };
+
   render() {
-    const { onItemAdded } = this.props;
     return (
-      <div className={'item-add-form'}>
-        <button
-          className="btn btn-outline-secondary"
-          onClick={() => onItemAdded('Hello world')}
-        >
-          Add Item
+      <form className={'item-add-form d-flex'} onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="What needs to be done"
+          onChange={this.onLabelChange}
+        />
+        <button className="btn btn-outline-secondary" onClick={() => {}}>
+          Add
         </button>
-      </div>
+      </form>
     );
   }
 }
